1af87c347cb122c19f40ee5c19683e85361b4a5e
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-05 23:18:11 +0530

    Added: method to delete a contact from the address book.

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -60,4 +60,21 @@ public class AddressBook {
             System.out.println("You entered a different name. ");
         }
     }
+    public void deleteContact() { //This method is to delete a contact by its first name.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first name of the contact to delete the contact :");
+        String Name = scanner.next();
+        if (contact.getFirstName().equalsIgnoreCase(Name)) {
+            System.out.println("The contact has deleted. ");
+            contact.setFirstName(null);
+            contact.setLastName(null);
+            contact.setAddress(null);
+            contact.setCity(null);
+            contact.setState(null);
+            contact.setZip(null);
+            contact.setPhoneNumber(null);
+            contact.setEmailId(null);
+
+        }
+    }
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -24,5 +24,14 @@ public class AddressBookMain {
         System.out.println(addressBook1.contact.getZip());
         System.out.println(addressBook1.contact.getPhoneNumber());
         System.out.println(addressBook1.contact.getEmailId());
+        addressBook1.deleteContact();
+        System.out.println(addressBook1.contact.getFirstName());
+        System.out.println(addressBook1.contact.getLastName());
+        System.out.println(addressBook1.contact.getAddress());
+        System.out.println(addressBook1.contact.getCity());
+        System.out.println(addressBook1.contact.getState());
+        System.out.println(addressBook1.contact.getZip());
+        System.out.println(addressBook1.contact.getPhoneNumber());
+        System.out.println(addressBook1.contact.getEmailId());
     }
 }
